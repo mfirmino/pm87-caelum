3f75559a88ff4fce3124a1904a69957fbb27aece
Marcelo Firmino <mfirmino@gmail.com>
2013-01-28 22:32:35 -0200

    alteracao do arquivo2

--- teste.java
+++ teste.java
@@ -4,6 +4,7 @@ public static void main(String[] args){
 
 System.out.println("Exemplo de teste de uso do git");
 System.out.println("Aula de Git");
+System.out.println("teste de push no repositorio");
 
 
 }
